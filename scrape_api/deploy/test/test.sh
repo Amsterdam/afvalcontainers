becda31243dc3fca9b3b3d9e606a4117799a203f
Stephan Preeker <stephan@preeker.net>
2018-04-19 15:25:50 +0200

    deploy meuk

---
+++ scrape_api/deploy/test/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+set -u
+set -x
+
+DIR="$(dirname $0)"
+
+dc() {
+	docker-compose -p cityd_import_test -f ${DIR}/docker-compose.yml $*
+}
+
+
+dc stop
+dc rm --force
+dc down
+dc pull
+dc build
+
+dc up -d database
+
+dc run --rm importer /app/deploy/docker-wait.sh
+dc run --rm importer pytest
+
+
+dc stop
+dc rm --force
+dc down
