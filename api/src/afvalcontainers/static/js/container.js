7a34c7d64e10e10b3c7d9e85eb2adcb6871e925f
Stephan Preeker <stephan@preeker.net>
2018-08-13 13:15:13 +0200

    jslint

--- api/src/afvalcontainers/static/js/container.js
+++ api/src/afvalcontainers/static/js/container.js
@@ -1,169 +1,172 @@
 (function() {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
+  // Create the connector object
+  var myConnector = tableau.makeConnector();
 
-    // Define the schema
-    myConnector.getSchema = function(schemaCallback) {
-        // Schema for magnitude and place data
-        var container_cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "id_number",
-            alias: "id number",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-	    id: "waste_name",
-            alias: "fractie",
-            dataType: tableau.dataTypeEnum.string
-	}, {
-	    id: "buurt_code",
-            alias: "buurt_code",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "active",
-            alias: "is active",
-            dataType: tableau.dataTypeEnum.boolean
-        }, {
-            id: "address",
-            alias: "address",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "lat",
-            alias: "latitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "lon",
-            alias: "longitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "volume",
-            alias: "volume m3",
-            dataType: tableau.dataTypeEnum.float
-	}, {
-            id: "site",
-            alias: "site",
-            dataType: tableau.dataTypeEnum.string
-	}, {
-            id: "placing_date",
-            alias: "placing date",
-            dataType: tableau.dataTypeEnum.date
-        }];
+  // Define the schema
+  myConnector.getSchema = function(schemaCallback) {
+    // Schema for magnitude and place data
+    var container_cols = [{
+      id: "id",
+      dataType: tableau.dataTypeEnum.string
+    },{
+      id: "id_number",
+      alias: "id number",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "waste_name",
+      alias: "fractie",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "buurt_code",
+      alias: "buurt_code",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "active",
+      alias: "is active",
+      dataType: tableau.dataTypeEnum.boolean
+    }, {
+      id: "address",
+      alias: "address",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "lat",
+      alias: "latitude",
+      dataType: tableau.dataTypeEnum.float
+    }, {
+      id: "lon",
+      alias: "longitude",
+      dataType: tableau.dataTypeEnum.float
+    }, {
+      id: "volume",
+      alias: "volume m3",
+      dataType: tableau.dataTypeEnum.float
+    }, {
+      id: "site",
+      alias: "site",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+      id: "placing_date",
+      alias: "placing date",
+      dataType: tableau.dataTypeEnum.date
+    }];
 
-        var containerTable = {
-            id: "container",
-            alias: "Container Data",
-            columns: container_cols
-	    //endpoint: "containers"
-        };
-
-        // Schema for time and URL data
-        var time_url_cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "time",
-            alias: "time",
-            dataType: tableau.dataTypeEnum.date
-        }, {
-            id: "url",
-            alias: "url",
-            dataType: tableau.dataTypeEnum.string
-        }];
-
-        //var timeUrlTable = {
-        //    id: "timeUrl",
-        //    alias: "Time and URL Data",
-        //    columns: time_url_cols
-        //};
-        schemaCallback([
-		containerTable,
-		//timeUrlTable
-	]);
+    var containerTable = {
+      id: "container",
+      alias: "Container Data",
+      columns: container_cols
+      //endpoint: "containers"
     };
 
-    // Download the data
-    myConnector.getData = async function(table, doneCallback) {
-        // var dateObj = JSON.parse(tableau.connectionData),
-        // dateString = "starttime=" + dateObj.startDate + "&endtime=" + dateObj.endDate,
+    // Schema for time and URL data
+    // var time_url_cols = [{
+    //   id: "id",
+    //   dataType: tableau.dataTypeEnum.string
+    // }, {
+    //   id: "time",
+    //   alias: "time",
+    //   dataType: tableau.dataTypeEnum.date
+    // }, {
+    //   id: "url",
+    //   alias: "url",
+    //   dataType: tableau.dataTypeEnum.string
+    // }];
 
-        var apiCall = "https://api.data.amsterdam.nl/afval/containers/";  // + dateString + "";
-        var apiCall = "http://localhost:8889/localhost:8000/afval/containers/";  // + dateString + "";
+    //var timeUrlTable = {
+    //    id: "timeUrl",
+    //    alias: "Time and URL Data",
+    //    columns: time_url_cols
+    //};
 
-        var params = {
-	    'format': 'json',
-	    'detailed': 1,
-	    'page_size': 1000,
-	    'page': 1,
-	}
+    schemaCallback([
+      containerTable,
+      //timeUrlTable
+    ]);
+  };
 
-        var page = 1;
-        var hasresults = [];
-	var rowcount = 0;
-	var feat = [];
-	var results = [];
+  // Download the data
+  myConnector.getData = function(table, doneCallback) {
+  // var dateObj = JSON.parse(tableau.connectionData),
+  // dateString = "starttime=" + dateObj.startDate + "&endtime=" + dateObj.endDate,
 
-	do {
+  //var apiCall = "https://api.data.amsterdam.nl/afval/containers/";  // + dateString + "";
+    var apiCall = "http://localhost:8889/localhost:8000/afval/containers/";  // + dateString + "";
 
-	  async function getPage(page) {
-
-	    params['page'] = page;
-
-  	    return $.getJSON(apiCall, params, function(resp) {
-
-	      feat = resp.results
-
-	      var tableData = [];
-	      var i = 0;
-	      var row = [];
-
-	      if( feat === undefined || feat.length === 0 ){
-	      } else {
-	        if (table.tableInfo.id == "container") {
-	          for (i = 0, len = feat.length; i < len; i++) {
-	            row = {
-	              "id": feat[i].id,
-	              "id_number": feat[i].id_number,
-	              "address": feat[i].address,
-	              "waste_name": feat[i].waste_name,
-	              "active": feat[i].active,
-	              "volume": feat[i].container_type.volume,
-	              "placing_date": feat[i].placing_date
-	            }
-
-	            if(feat[i].well != null) {
-	              row["lon"] = feat[i].well.geometrie.coordinates[0];
-	              row["lat"] = feat[i].well.geometrie.coordinates[1];
-	              row["buurt_code"] = feat[i].well.buurt_code;
-	              row["site"] = feat[i].well.site;
-	            }
-	            tableData.push(row);
-	            rowcount += 1;
-	          }
-	        }
-	        table.appendRows(tableData);
-	      }
-	  });
-	}
-
-	results = await getPage(page);
-	page += 1;
-        // get the next page.
-      	// as long as we get response continue loading!
-      } while(results.results.length && page < 14);
-
-      // update loop runaway failsafe.
-      doneCallback();
-      console.log(rowcount);
+    var params = {
+      "format": "json",
+      "detailed": 1,
+      "page_size": 1000,
+      "page": 1,
     };
 
-    tableau.registerConnector(myConnector);
+    var page = 1;
+    //var hasresults = [];
+    var rowcount = 0;
+    var feat = [];
+    var results = [];
 
-    // Create event listeners for when the user submits the form
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-	    tableau.connectionName = "Containers"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
-        });
+    function getPage(page) {
+
+      params.page = page;
+
+      $.getJSON(apiCall, params, function(resp) {
+
+        feat = resp.results;
+
+        var tableData = [];
+        var i = 0;
+        var row = [];
+        var len = 0;
+
+        if( feat === undefined || feat.length === 0 ){
+          return;
+        } else {
+          if (table.tableInfo.id == "container") {
+            for (i = 0, len = feat.length; i < len; i++) {
+              row = {
+                "id": feat[i].id,
+                "id_number": feat[i].id_number,
+                "address": feat[i].address,
+                "waste_name": feat[i].waste_name,
+                "active": feat[i].active,
+                "volume": feat[i].container_type.volume,
+                "placing_date": feat[i].placing_date
+              };
+
+              if(feat[i].well !== null) {
+                row.lon = feat[i].well.geometrie.coordinates[0];
+                row.lat = feat[i].well.geometrie.coordinates[1];
+                row.buurt_code = feat[i].well.buurt_code;
+                row.site = feat[i].well.site;
+              }
+              tableData.push(row);
+              rowcount += 1;
+            }
+          }
+          table.appendRows(tableData);
+          return;
+        }
+      });
+    }
+
+    do {
+      results = getPage(page);
+      page += 1;
+      // get the next page.
+      // as long as we get response continue loading!
+    } while(results.results.length && page < 14);
+
+    // update loop runaway failsafe.
+    doneCallback();
+    console.info.log(rowcount);
+  };
+
+  tableau.registerConnector(myConnector);
+
+  // Create event listeners for when the user submits the form
+  $(document).ready(function() {
+    $("#submitButton").click(function() {
+      tableau.connectionName = "Containers"; // This will be the data source name in Tableau
+      tableau.submit(); // This sends the connector object to Tableau
     });
+  });
 })();
