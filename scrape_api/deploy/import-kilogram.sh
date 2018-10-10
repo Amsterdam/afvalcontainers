4b881ba5a7e1d05c70fc25e6eebd2b69ac428047
Stephan Preeker <stephan@preeker.net>
2018-10-10 14:05:20 +0200

    add kilogram import script and endpoint

---
+++ scrape_api/deploy/import-kilogram.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -u   # crash on missing env variables
+set -e   # stop on any error
+set -x   # print what we are doing
+
+
+cd /app/
+
+# create database tables if not exists.
+python kilogram/models.py
+
+# slurp latest and greatest of kilogram.nl
+python kilogram/slurp.py
+
+# copy data into final table for serving to django
+python kilogram/copy_to_model.py
+
