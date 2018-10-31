8264e7aefc050a86d96a8d28977e26717fc135a1
Stephan Preeker <stephan@preeker.net>
2018-10-31 11:11:11 +0100

    sql cleanup / fix

--- scrape_api/bammens/sqlcode/capacity_to_sitestats.sql
+++ scrape_api/bammens/sqlcode/capacity_to_sitestats.sql
@@ -1,20 +1,20 @@
 UPDATE kilogram_sitefractiestatweek k set
-	volume=f.volume,
+	volume=f.volume_m3,
 	containers=f.containers
 from afvalcontainers_sitefractie f, afvalcontainers_site s
 WHERE
 	k.site_id = s.short_id
 AND	s.id = f.site_id
 AND	(f.fractie = k.fractie or
-	 (k.fractie = 'Kunststof' and f.fractie = 'Plastic'))
+	 (k.fractie = 'Kunststof' and f.fractie = 'Plastic'));
 
 
 UPDATE kilogram_sitefractiestatmonth k set
-	volume=f.volume,
+	volume=f.volume_m3,
 	containers=f.containers
 from afvalcontainers_sitefractie f, afvalcontainers_site s
 WHERE
 	k.site_id = s.short_id
 AND	s.id = f.site_id
 AND	(f.fractie = k.fractie or
-	 (k.fractie = 'Kunststof' and f.fractie = 'Plastic'))
+	 (k.fractie = 'Kunststof' and f.fractie = 'Plastic'));
