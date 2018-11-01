4b9052b8d71e6c8049fcbbb1d469a516165f0fb8
Stephan Preeker <stephan@preeker.net>
2018-11-01 15:41:42 +0100

    make sure all 100% of sites have a short id

--- scrape_api/bammens/sqlcode/create_short_ids.sql
+++ scrape_api/bammens/sqlcode/create_short_ids.sql
@@ -1,11 +1,25 @@
 /* Create short ids based on (part off) streetcode and housenumber
 
    If there are multiple sites for street and housenumber add a rownumber
-   this is true in ~200 cases. parks, long weird streets with no houses.
+   this is true in ~200 cases. which could happend in parks and
+   long weird streets with no houses.
 
-   Also copy adress summary information / chauffeur instrictions to the site.
+   Also copy adress summary information / chauffeur instructions to the site.
 */
 
+/*
+
+	Create short id for most common case.
+
+	For each site, find one well (cross join) ,
+	and join with opr / openbare ruimte on the straatnaam.
+
+	now we have all information to crate a short id.
+
+	rown must be 1. so in case multiple sites share the same adress
+	we take only the first one.
+
+*/
 
 update afvalcontainers_site s set
         short_id = short.short_id,
@@ -35,6 +49,16 @@ where o.opr_type = 'Weg'
 ) as short
 where short.rown = 1 and short.site_id = s.id;
 
+/*
+
+	Same as above but now we do all left over cases in which
+	sites SHARE the same streetnamen and housenumber.
+
+	In this case where rownumber > 1 we add the rownumber to the short id
+	assuming this will be unique.
+
+*/
+
 
 update afvalcontainers_site s set
         short_id = cast(concat(rown, short.short_id) as int),
@@ -56,18 +80,25 @@ from afvalcontainers_site s
 left outer join openbareruimte o on(s.straatnaam = o.display)
 cross join lateral (select * from afvalcontainers_well w where s.id = w.site_id limit 1) as w
 where o.opr_type = 'Weg'
+and s.short_id is null
 ) as short
 where short.rown > 1
 and short.site_id = s.id;
 
+
+/*
+
+Some sites have no well. We still want to create a short id for them.
+
+*/
 
 update afvalcontainers_site s set
 	short_id = cast(concat(rown, short.short_id) as int)
 from (
 select
     row_number() over (partition by s.straatnaam, s.huisnummer order by s.id) as rown,
-	  concat(
-		right(o.id, 5),
+    concat(
+	right(o.id, 5),
 	huisnummer
     ) as short_id,
     s.id as site_id,
@@ -80,4 +111,47 @@ left outer join openbareruimte o on(s.straatnaam = o.display)
 where o.opr_type = 'Weg'
 and s.short_id is null
 ) as short
+where short.site_id = s.id;
+
+
+/*
+
+	Some sites are missing an OPR with valid numbers
+
+	5 cases in molenwijk. (the building is the actual street.)
+
+	In this case we take nearest opr + housenumer rownumber > 1 we add the rownumber to the short id
+	assuming this will be unique.
+
+*/
+
+update afvalcontainers_site s set
+        short_id = cast(concat(rown, short.short_id) as int),
+        extra_attributes = jsonb_build_object('chauffeur', short.chauffeur)
+from (
+select
+    row_number() over (partition by opr.display, s.huisnummer order by s.id) as rown,
+    concat(
+        right(opr.id, 4),
+        huisnummer
+    ) as short_id,
+    s.id as site_id,
+    straatnaam,
+    huisnummer,
+    distance,
+    opr.id as code,
+    w.address->>'summary' as chauffeur
+from afvalcontainers_site s
+cross join lateral (select * from afvalcontainers_well w where s.id = w.site_id limit 1) as w
+cross join lateral
+	(select
+		o.id,
+		o.display
+	from openbareruimte o, bag_verblijfsobject v
+	where o.opr_type = 'Weg'
+	order by
+		o.wkb_geometry <-> s.geometrie
+	limit 1) as opr
+) as short
 where short.site_id = s.id
+and s.short_id is null;
