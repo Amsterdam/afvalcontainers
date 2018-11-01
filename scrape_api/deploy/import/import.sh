90b58171f4ce0e5d6049b4ba6181f3d9d8778268
sercanyaldiz <s.yaldiz@amsterdam.nl>
2018-11-01 08:49:59 +0100

    remove the docker-netwerk with trap (#9)

--- scrape_api/deploy/import/import.sh
+++ scrape_api/deploy/import/import.sh
@@ -15,7 +15,7 @@ dc() {
 	docker-compose -p scrapebammens${ENVIRONMENT} -f ${DIR}/docker-compose.yml $*
 }
 
-trap 'dc kill ; dc rm -f' EXIT
+trap 'dc kill ; dc down ; dc rm -f' EXIT
 
 # For database backups:
 rm -rf ${DIR}/backups
