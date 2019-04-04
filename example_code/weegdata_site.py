5c0b11ead926c84abd414d28a43e24cd2ce99bf9
Stephan Preeker <stephan@preeker.net>
2019-04-04 17:18:31 +0200

    add api example code

--- example_code/weegdata_site.py
+++ example_code/weegdata_site.py
@@ -5,15 +5,22 @@ import logging
 
 # korte site codes. straatcode + huisnummer + oploopummer
 BASE_DIR = os.path.dirname(__file__)
-BASE_API = "https://api.data.amsterdam.nl/afval/"
-BASE_API = "http://127.0.0.1:8000/afval/"
+BASE_API = "https://api.data.amsterdam.nl/afval"
+BASE_API = "http://127.0.0.1:8000/afval"
 
 # directory met alle doeldata.
 DATA_PATH = os.path.abspath(os.path.join(BASE_DIR, "weegdata"))
 print(DATA_PATH)
 
-FRACTIE = 'REST'
+FRACTIE = ''  # Alle fracties
+# FRACTIE = 'Rest'
+# FRACTIE = 'Glas'
+# FRACTIE = 'Papier'
+# FRACTIE = 'Textiel'
+# FRACTIE = 'Plastic'
 
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.ERROR)
 log = logging.getLogger("weegdata")
 log.setLevel(logging.DEBUG)
 
@@ -21,7 +28,6 @@ log.setLevel(logging.DEBUG)
 def site_ids():
     sites_source = open('sites_poc.txt', 'r')
     site_ids = sites_source.readlines()
-
     return site_ids
 
 
@@ -32,30 +38,50 @@ def get_centroid(short_id):
     url = f"{BASE_API}/v1/sites/?short_id={short_id}"
 
     response = requests.get(url)
-    print(response)
     if response.status_code != 200:
         log.error('api call error %s', response.text)
         return None, None
     #
     data = response.json()
+    results = data['results']
+    if not results:
+        log.error('site missing %s', short_id)
+        return None, None
     lon, lat = data['results'][0]['centroid']['coordinates']
     return lon, lat
 
 
 def weegdata(short_id, lon, lat, fractie):
     """
-    Get all weegdata for a site
+    Get all weegdata for a site.
+
+    We look for all measurements near a site
     """
-    r = 0.005
-    url = f"{BASE_API}/suppliers/kilogram/?location={lat},{lon},{r}"
-    print(url)
-    #
-    response = requests.get(url)
+    r = 17  # 17 meter.
+    url = f"{BASE_API}/suppliers/kilogram/"
+
+    params = {
+        "location": f"{lon},{lat},{r}",
+        "page_size": 15000,
+        "format": "csv",
+        # "fractie": "Rest",
+    }
+    if FRACTIE:
+        params['fractie'] = FRACTIE
+    response = requests.get(url, params=params)
     if response.status_code != 200:
         log.error('api call error %s', response.text)
         return
     #
-    data = response.json()
+    data = response.text
+    fractie_name = 'allefracties'
+    if FRACTIE:
+        fractie_name = FRACTIE
+    csv_file = f"weegdata_{fractie_name}_{lon:.5f}_{lat:.5f}_{r}m_site_{short_id}.csv"   # noqa
+    log.info('Writing to %s', csv_file)
+    weegdatacsv = os.path.join(DATA_PATH, csv_file)
+    target = open(weegdatacsv, 'w')
+    target.write(data)
 
 
 def download_weegdata():
@@ -65,7 +91,8 @@ def download_weegdata():
 
     all_site_ids = site_ids()
 
-    for short_id in all_site_ids[:5]:
+    for short_id in all_site_ids:
+        short_id = short_id.strip()
         lon, lat = get_centroid(short_id)
         if not lon:
             continue
