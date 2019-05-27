a93d3ae42a38818890acf485a869de502f807aff
Pieter van Beek <pieter@djinnit.com>
2019-05-27 15:20:48 +0200

    Removed stale authorization dependency

--- api/src/settings/settings_common.py
+++ api/src/settings/settings_common.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # 'authorization_django.authorization_middleware',
 ]
 
 X_FRAME_OPTIONS = 'ALLOW-FROM *'
