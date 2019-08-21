15c3850a6186fbd35e46d1a9a2acf34c1b1d321a
Niels Lensink <niels@we-riot.com>
2019-08-21 08:45:33 +0200

    renamed jenkinsfile to validate.groovy

--- scrape_api/deploy/validate.groovy
+++ scrape_api/deploy/validate.groovy
@@ -1,22 +1,5 @@
 #!groovy
 
-def tryStep(String message, Closure block, Closure tearDown = null) {
-    try {
-        block()
-    }
-    catch (Throwable t) {
-        slackSend message: "${env.JOB_NAME}: ${message} failure ${env.BUILD_URL}", channel: '#ci-channel', color: 'danger'
-
-        throw t
-    }
-    finally {
-        if (tearDown) {
-            tearDown()
-        }
-    }
-}
-
-
 node {
     stage("Checkout") {
         checkout scm
