f052a28952080031a1cda95f87664b15a930ace3
Niels Lensink <niels@we-riot.com>
2019-08-21 12:36:06 +0200

    use slack webhook

--- scrape_api/kilogram/validate.py
+++ scrape_api/kilogram/validate.py
@@ -1,12 +1,38 @@
+import json
 import os
 
-import slack
+import requests
 
-client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
+webhook_url = os.environ['SLACK_WEBHOOK_URL']
 
-response = client.chat_postMessage(
+data = dict(
     channel='#niels-test',
-    text="Hello world!",
+    username="afvalcontainers-bot",
+    blocks=[{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
+        }
+    }, {
+        "type": "divider"
+    }, {
+        "type": "section",
+        "text": {
+            "type": "plain_text",
+            "text": "This is a plain text section block.",
+            "emoji": True
+        }
+    }]
 )
-assert response["ok"]
-assert response["message"]["text"] == "Hello world!"
+response = requests.post(
+    webhook_url,
+    data=json.dumps(data),
+    headers={'Content-Type': 'application/json'}
+)
+
+if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s' %
+        (response.status_code, response.text)
+    )
