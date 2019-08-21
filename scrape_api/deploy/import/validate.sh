e258aae867301908cf4f057bcbf163cbc9f39942
Niels Lensink <niels@we-riot.com>
2019-08-21 09:13:02 +0200

    test slack from bash script

--- scrape_api/deploy/import/validate.sh
+++ scrape_api/deploy/import/validate.sh
@@ -4,4 +4,6 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x   # print what we are doing
 
-echo 'Check!'
+echo 'Check!' > log.txt
+cat log.txt
+curl -F file=log.txt -F channels=niels-test  -H "Authorization: ${SLACK_ID}" https://slack.com/api/files.upload
